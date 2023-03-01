fd15e864540c4c75beba874bd9922f5fc55505c0
padma <padmalayaratha1200@gmail.com>
2023-03-01 21:59:17 +0530

    Stack data structure

--- Stack.java
+++ Stack.java
@@ -1,84 +1,93 @@
 import java.util.Scanner;
+
 public class Stack {
     int size = 5;
     int[] arr = new int[size];
     int top = -1;
-    int choice;
 
-    void push(int value) {
+    void push(int data) {
         if (top == size - 1) {
             System.out.println("Stack is full\n");
+            System.exit(0);
         } else {
             top++;
-            Scanner sc = new Scanner(System.in);
-            System.out.println(arr[size]);
-
-            String element= sc.nextLine();
-            System.out.println("Element is:"+element);
-
+            arr[top] = data;
+            System.out.println("your  element is " + data);
         }
     }
-    void pop(){
-        if(top == -1){
+
+    void pop() {
+        if (top == -1) {
             System.out.println("Stack is empty so pop operation not possible.");
-        }
-        else{
-
-            System.out.println("Delete element successfully.\n");
+        } else {
             top--;
+            System.out.println("Delete element successfully.\n");
         }
     }
-    void peek(){
-        if(top == -1){
+
+    void peek() {
+        if (top == -1) {
             System.out.println("Stack is empty. ");
-        }
-        else{
+        } else {
             System.out.println("Top of the peek element is:\n");
+            arr[top] = size - 1;
         }
     }
-    void display(){
-        for(int i = 0;i <= 5;i++){
+
+    void display() {
+        for (int i = 0; i <= 5; i++) {
             System.out.println("Your stack elements are:\n");
             System.out.println(arr[i]);
         }
     }
-    void isEmpty(){
-        if(top == -1){
+
+    void isEmpty() {
+        if (top == -1) {
             System.out.println("Stack is empty.\n");
-        }
-        else{
+        } else {
             System.out.println("Stack is full.\n");
         }
     }
-    void isFull(){
-        if(top == size-1){
+
+    void isFull() {
+        if (top == size - 1) {
             System.out.println("Stack is full.\n");
-        }
-        else{
+        } else {
             System.out.println("stack is empty.\n");
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("Enter your choice\n1.push\n2.pop\n3.peek\n4.display\n5.isEmpty\n6.isFull");
         Stack ss = new Stack();
-        if (ss.choice == 1) {
-            System.out.println("Enter the choice to push into stack.");
-            ss.push(10);
-        } else if (ss.choice == 2) {
-            ss.pop();
-        } else if (ss.choice == 3) {
-            ss.peek();
-        } else if (ss.choice == 4) {
-            ss.display();
-        } else if (ss.choice == 5) {
-            ss.isEmpty();
-        } else if(ss.choice == 6) {
-            ss.isFull();
-        }
-        else{
+        System.out.println("Enter your choice\n1.push\n2.pop\n3.peek\n4.display\n5.isEmpty\n6.isFull");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
 
-        }
+        while (true) {
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter your data:");
+                    int data = scanner.nextInt();
+                    ss.push(data);
+                    break;
+                case 2:
+                    System.out.println("Remove your data:");
+                    ss.pop();
+                    break;
+                case 3:
+                    ss.peek();
+                    break;
+                case 4:
+                    ss.display();
+                    break;
+                case 5:
+                    ss.isFull();
+                    break;
+                case 6:
+                    ss.isEmpty();
+                    break;
 
+            }
+        }
     }
 }
